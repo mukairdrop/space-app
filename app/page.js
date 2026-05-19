a4eb28dacee17123149f0cff7fe678f7c71f732c
mukairdrop <145890011+mukairdrop@users.noreply.github.com>
2026-05-19 14:01:07 +0530

    Refactor Home component to display new version message

--- app/page.js
+++ app/page.js
@@ -1,30 +1,3 @@
-"use client";
-
-import { useState } from "react";
-
 export default function Home() {
-const [data, setData] = useState("");
-
-const getRandom = async () => {
-const res = await fetch("/api/random");
-const json = await res.json();
-
-```
-setData(JSON.stringify(json));
-```
-
-};
-
-return ( <main> <h1>SpaceComputer Demo</h1>
-
-```
-  <button onClick={getRandom}>
-    Generate Random
-  </button>
-
-  <div>{data}</div>
-</main>
-```
-
-);
+return <h1>NEW VERSION WORKING</h1>;
 }
