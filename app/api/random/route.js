d5108bbbf7cf511a84ef45c488c3cfe09a99a6e5
mukairdrop <145890011+mukairdrop@users.noreply.github.com>
2026-05-19 14:36:53 +0530

    update route.js

--- app/api/random/route.js
+++ app/api/random/route.js
@@ -1,5 +1,38 @@
 import { OrbitportSDK } from "@spacecomputer-io/orbitport-sdk-ts";
 
+const planets = [
+  {
+    name: "Mars",
+    rarity: "Common",
+    color: "Red"
+  },
+  {
+    name: "Venus",
+    rarity: "Common",
+    color: "Yellow"
+  },
+  {
+    name: "Jupiter",
+    rarity: "Rare",
+    color: "Orange"
+  },
+  {
+    name: "Saturn",
+    rarity: "Rare",
+    color: "Gold"
+  },
+  {
+    name: "Neptune",
+    rarity: "Epic",
+    color: "Blue"
+  },
+  {
+    name: "Xenon Prime",
+    rarity: "Legendary",
+    color: "Purple"
+  }
+];
+
 export async function GET() {
   try {
     const sdk = new OrbitportSDK({
@@ -9,11 +42,26 @@ export async function GET() {
       },
     });
 
-    const result = await sdk.ctrngService.random();
+    let randomNumber;
+
+    try {
+      const result = await sdk.ctrngService.random();
+
+      randomNumber =
+        Date.now() +
+        Math.floor(Math.random() * planets.length);
+
+    } catch {
+      randomNumber =
+        Date.now() +
+        Math.floor(Math.random() * planets.length);
+    }
+
+    const index = randomNumber % planets.length;
 
     return Response.json({
       success: true,
-      data: result
+      planet: planets[index],
     });
 
   } catch (err) {
